ce7e4d0cc9b832aa45ebd7ae74ba29fc950276d7
EC2 Default User <ec2-user@ip-172-31-4-81.ap-southeast-1.compute.internal>
2023-10-24 11:26:03 +0000

    testing

---
+++ archived-machine-learning-code/recommendCandidatesToRecruiter/app.py
@@ -0,0 +1,151 @@
+import requests
+import random
+import boto3
+import json
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Define the function name and parameters
+def recommend_candidates_to_recruiter(event, context):
+  # Validate input API arguments
+  search_criteria = event.get("SearchCriteria", None) # Search Criteria as string
+  job_id = event.get("JobId", None) # Jobid as string
+  num_recommended = event.get("NumOfCandidates", 5) # Number of recommended candidates as integer, default to 5
+  
+  # If Search Criteria argument is provided, use it as job description and generate random search serial number
+  if search_criteria:
+    job_description = search_criteria
+    job_requirements = None
+    job_id = "candidateSrchId-" + str(random.randint(10**11, 10**12 - 1)) # Generate random 12-digit number and prefix with srchId-
+  
+  # If Jobid argument is provided, retrieve the job data from Job Service API and use the description and job_type_id fields
+  elif job_id:
+    # Define Stack Name of Job Service API Resource
+    stack_name = 'dev-app'
+    api_res_name = 'JobDbDevApi'
+    api_url = get_apiurl_frmcfn(stack_name, api_res_name)
+    # Retrieve the Job data from Job Service API
+    job_service_url = api_url + '/' + job_id # Construct the API URL with the job_id
+    api_response = requests.get(job_service_url) # Get the JSON response and access the job field
+    if api_response.status_code == 200:
+      api_output = api_response.json()
+      job_data = {}
+      job_data['data'] = json.loads(api_output['data'])
+      job_description = job_data['data']['description'] + ' ' + job_data['data']['requirements']# Get the description field
+      #job_requirements = job_data['data']['requirements'] # Get the job_type_id field
+    else:
+      return { "message": f"JobServiceApi Request failed with status code {api_response.status_code}.", "api_response": api_response.json() }
+    #return job_data, job_description, job_requirements #For Debugging
+    #return { "api_output": api_output, "job_service_url": job_service_url } #For Debugging
+  
+  # If neither argument is provided, return an error message
+  else:
+    return {"error": "Please provide either Search Criteria or Jobid as input arguments"}
+    #return {"error": "Please provide either Search Criteria or Jobid as input arguments", **event, "search_criteria": search_criteria, "job_id": job_id} #For Debugging
+  
+  # Define Stack Name of Candidate Service API Resource
+  stack_name = 'dev-app'
+  api_res_name = 'CandiDbDevApi'
+  api_url = get_apiurl_frmcfn(stack_name, api_res_name)
+  #return api_url #For Debugging
+  
+  # Retrieve the list of candidate data from Candidate Service API
+  candidate_service_url = api_url # The API URL for candidate service. Optional to Construct with API parameters
+  #return candidate_service_url #For Debugging
+
+  api_response = requests.get(candidate_service_url) # Get the JSON response as a list of candidates
+  if api_response.status_code == 200:
+    api_output = api_response.json()
+    candidate_list = {}
+    candidate_list['data'] = json.loads(api_output['data']) # Get the JSON response as a list of candidates
+  else:
+    return { "message": f"CandidateServiceApi Request failed with status code {api_response.status_code}.", "api_response": api_response.json() }
+  #return candidate_list #For Debugging
+
+  # Extract the candidate ids, skills and resumes from the candidate list
+  # Ref-code1: candidate_ids = [candidate["data"]["id"] for candidate in candidate_list] # A list of candidate ids | id is in type 'integer'
+  # Ref-code2: candidate_ids = [json.loads(candidate)["data"]["id"] for candidate in candidate_list] # A list of candidate ids | converted candidate in to dic
+  candidate_ids = [candidate['id'] for candidate in candidate_list['data']]
+  #return #For Debugging
+
+  # Ref-code1: candidate_skills = [", ".join(candidate["data"]["skills"]) for candidate in candidate_list] # A list of candidate skills as comma-separated strings 
+  # Ref-code2: candidate_skills = [", ".join(candidate['skills']) for candidate in candidate_list['data']]
+  # Ref-code3: candidate_skills = [", ".join(candidate['skills']).replace(',', ' ') for candidate in candidate_list['data']]
+  candidate_skills = [" ".join(candidate['skills'] + [' ', candidate['education']]).replace(',', ' ') for candidate in candidate_list['data']]
+  #return candidate_skills #For Debugging
+
+  #candidate_resumes = [candidate["data"]["resumeUrl"] for candidate in candidate_list] # A list of candidate resume URLs
+  #return { "candidate_ids": candidate_ids, "candidate_skills": candidate_skills } #For Debugging
+  
+  # Create a TF-IDF vectorizer to transform the text data into numerical features
+  vectorizer = TfidfVectorizer()
+  
+  # Fit the vectorizer on the job description and the candidate skills
+  vectorizer.fit([job_description] + candidate_skills)
+  
+  # Transform the job description and the candidate skills into TF-IDF vectors
+  job_vector = vectorizer.transform([job_description]) # A sparse matrix with one row and n_features columns
+  candidate_vectors = vectorizer.transform(candidate_skills) # A sparse matrix with n_candidates rows and n_features columns
+  
+  # Compute the cosine similarity between the job vector and each candidate vector
+  similarity_scores = np.dot(candidate_vectors, job_vector.T).toarray().flatten() # A numpy array with n_candidates elements
+
+  # Sort the candidate ids and similarity scores by descending order of similarity scores
+  sorted_indices = np.argsort(similarity_scores)[::-1] # A numpy array with n_candidates elements indicating the sorted positions
+  sorted_candidate_ids = [candidate_ids[i] for i in sorted_indices] # A list of candidate ids sorted by similarity scores
+  sorted_similarity_scores = [similarity_scores[i] for i in sorted_indices] # A list of similarity scores sorted by similarity scores
+  #return sorted_similarity_scores #For Debugging
+  
+  # Generate a recommendation id as an alphanumeric serial number
+  recommendation_id = "".join(random.choices("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", k=10)) # A random string of length 10
+  
+  # Create a list of dictionaries to store the output information for each recommended candidate
+  output_list = []
+  
+  # Loop through the number of recommended candidates and append the output information to the output list
+  for i in range(num_recommended):
+    output_dict = {
+      "Recommendation-Id": recommendation_id,
+      "JobId/SearchId": job_id,
+      "Recommended Candidate-Id": sorted_candidate_ids[i],
+      "Rank": i + 1,
+      "Similarity Score": "{:.2f}%".format(sorted_similarity_scores[i] * 100) # Format the similarity score as a percentage with two decimal places
+    }
+    output_list.append(output_dict)
+  
+  # Return the output list as the API response
+  return {
+    "statusCode": 200,
+    "message": "List of Recommended Candidates",
+    "body": output_list
+  }
+
+'''
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+^^^ THIS IS END OF THE MAIN LAMBDA FUNCTION ^^^
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+###############################################
+## Other Function Declaration Section Start ###
+###############################################
+'''
+
+def get_apiurl_frmcfn(stack_name, output_key):
+    # Create a CloudFormation client
+    cfn_client = boto3.client('cloudformation')
+
+    try:
+        # Describe the stack to retrieve its outputs
+        response = cfn_client.describe_stacks(StackName=stack_name)
+        stack = response['Stacks'][0]
+
+        # Extract the outputs as a dictionary
+        outputs = {output['OutputKey']: output['OutputValue'] for output in stack['Outputs']}
+
+        # Check if the specified output key exists
+        if output_key in outputs:
+            return outputs[output_key]
+        else:
+            return f"Output key '{output_key}' not found in the stack outputs."
+
+    except Exception as e:
+        return str(e)
