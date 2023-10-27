b3c0232373907fb13aec257cc5fbddf6b3003a51
EC2 Default User <ec2-user@ip-172-31-4-81.ap-southeast-1.compute.internal>
2023-10-27 12:40:36 +0000

    change job service production url

--- recommendCandidatesToRecruiter/app.py
+++ recommendCandidatesToRecruiter/app.py
@@ -74,7 +74,8 @@ def recommend_candidates_to_recruiter(event, context):
     }
 
 def get_job_service_url(job_id):
-    return 'http://job-service-32b5298bca7abe46.elb.ap-southeast-1.amazonaws.com/api/v1/jobs/' + job_id
+    #return 'http://job-service-32b5298bca7abe46.elb.ap-southeast-1.amazonaws.com/api/v1/jobs/' + job_id
+    return 'http://job-service-elb-de53cd8b61f351ee.elb.ap-southeast-1.amazonaws.com/api/v1/jobs/' + job_id
 
 def get_job_description(job_service_url):
     api_response = requests.get(job_service_url)
